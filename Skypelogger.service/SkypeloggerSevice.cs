409d851106094fc4ca9140f4e6cd891f12ac81f4
Kristoffer Risa <kristofferrisa@gmail.com>
2017-12-05 17:28:59 +0100

    added a project for hosting skypelogger as a windows service

---
+++ Skypelogger.service/SkypeloggerSevice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ServiceProcess;
+using Microsoft.Lync.Model;
+using Polly;
+
+namespace Skypelogger.service
+{
+    public class SkypeloggerSevice : ServiceBase
+    {
+        static LyncClient _client;
+        private readonly core.SkypeloggerManager _skypeloggerManager;
+
+        public SkypeloggerSevice()
+        {
+            _client = null;
+            _skypeloggerManager = new core.SkypeloggerManager();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            var policy = Policy.Handle<LyncClientException>()
+                .WaitAndRetryForever(retryAttempt =>
+                    TimeSpan.FromSeconds(2)
+                );
+
+            try
+            {
+                policy.Execute(() =>
+                {
+                    _client = LyncClient.GetClient();
+                    _client.ConversationManager.ConversationAdded += _skypeloggerManager.AddConversation;
+                    _client.ConversationManager.ConversationRemoved += _skypeloggerManager.RemoveConversation;
+                });
+            }
+            catch (Exception e)
+            {
+                Stop();
+            }
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
